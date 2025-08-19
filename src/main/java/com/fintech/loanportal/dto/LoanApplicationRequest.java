d639069e8e86c68142d8077bcf864f2a3d4a216b
hopekali04 <hopekalitera@gmail.com>
2025-08-19 14:31:31 +0200

    feat: add LoanApplicationRequest DTO for loan application data handling

---
+++ src/main/java/com/fintech/loanportal/dto/LoanApplicationRequest.java
@@ -0,0 +1,33 @@
+package com.fintech.loanportal.dto;
+
+import java.math.BigDecimal;
+
+public class LoanApplicationRequest {
+    private BigDecimal loanAmount;
+    private int loanTermMonths;
+    private BigDecimal interestRate;
+
+    public BigDecimal getLoanAmount() {
+        return loanAmount;
+    }
+
+    public void setLoanAmount(BigDecimal loanAmount) {
+        this.loanAmount = loanAmount;
+    }
+
+    public int getLoanTermMonths() {
+        return loanTermMonths;
+    }
+
+    public void setLoanTermMonths(int loanTermMonths) {
+        this.loanTermMonths = loanTermMonths;
+    }
+
+    public BigDecimal getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(BigDecimal interestRate) {
+        this.interestRate = interestRate;
+    }
+}
