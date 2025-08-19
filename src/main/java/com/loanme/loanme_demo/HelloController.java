7876214c2abb46b6bd3972ac50fcc19e0d4be307
hopekali04 <hopekalitera@gmail.com>
2025-08-19 10:22:22 +0200

    feat: initial commit

--- src/main/java/com/loanme/loanme_demo/HelloController.java
+++ src/main/java/com/loanme/loanme_demo/HelloController.java
@@ -1,18 +1,7 @@
 package com.loanme.loanme_demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-// @SpringBootApplication
-// public class LoanmeDemoApplication {
-
-// 	public static void main(String[] args) {
-// 		SpringApplication.run(LoanmeDemoApplication.class, args);
-// 	}
-
-// }
 
 @RestController
 public class HelloController {
---
+++ src/main/java/com/loanme/loanme_demo/LoanmeDemoApplication.java
@@ -0,0 +1,15 @@
+package com.loanme.loanme_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanmeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanmeDemoApplication.class, args);
+	}
+
+}
+
+
